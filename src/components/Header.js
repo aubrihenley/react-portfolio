33ca22d371d6a0752e4838d6c53816596a59c913
Aubri Henley <aubri.henley@gmail.com>
2021-10-24 17:00:01 -0400

    fixed navlink styling when active

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../main.css";
 import "bulma/css/bulma.min.css";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
@@ -14,26 +15,38 @@ export default function Header() {
           </div>
         </div>
         <div className="hero-foot ">
-          <div className="hero-foot--wrapper">
+          <nav className="hero-foot--wrapper">
             <div className="columns">
               <div className="column is-12 hero-menu-desktop has-text-centered">
                 <ul>
                   <li>
-                    <a href="/">About Me</a>
+                    <NavLink exact to="/" activeClassName="active-link">
+                      About Me
+                    </NavLink>
                   </li>
                   <li>
-                    <a href="/portfolio">Portfolio</a>
+                    <NavLink
+                      exact
+                      to="/portfolio"
+                      activeClassName="active-link"
+                    >
+                      Portfolio
+                    </NavLink>
                   </li>
                   <li>
-                    <a href="/resume">Resume</a>
+                    <NavLink exact to="/resume" activeClassName="active-link">
+                      Resume
+                    </NavLink>
                   </li>
                   <li>
-                    <a href="/contact">Contact</a>
+                    <NavLink exact to="/contact" activeClassName="active-link">
+                      Contact
+                    </NavLink>
                   </li>
                 </ul>
               </div>
             </div>
-          </div>
+          </nav>
         </div>
       </section>
     </div>

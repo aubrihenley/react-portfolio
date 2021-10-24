8768e08d6c459383016107bf341fbe7e95029c9b
Aubri Henley <aubri.henley@gmail.com>
2021-10-24 15:51:04 -0400

    fixing app routes, imported all components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+//import PortfolioContainer from "./components/PortfolioContainer";
 import "bulma/css/bulma.min.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Portfolio from "./components/pages/Portfolio";
+import Resume from "./components/pages/Resume";
 import "./main.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <div className="content">
-        <PortfolioContainer />
+    <router>
+      <div className="App">
+        <Header />
+        <div className="content"></div>
+
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </router>
   );
 };
 
